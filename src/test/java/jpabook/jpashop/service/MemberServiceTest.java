9de7c030c5a49546bdd3aa062990f64a19d50a42
MINU234 <kmw365849@gmail.com>
2024-12-18 03:22:10 +0900

    회원 기능 테스트

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,7 +1,47 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MemberServiceTest {
 
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("kim");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Assertions.assertThat(member).isEqualTo(memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_에러() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+        
+        memberService.join(member1);
+//        memberService.join(member2);
+        
+        fail("예외발생");
+    }
 }
