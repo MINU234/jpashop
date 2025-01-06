f1ecdc8242eb29da99bbd661dbf0617beb91774a
MINU234 <kmw365849@gmail.com>
2025-01-07 00:28:16 +0900

    JPA 스프링부트 실전 2) Entity 직접 노출

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-hibernate5")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-hibernate5-jakarta")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +11,9 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
-
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule hibernate5Moudle= new Hibernate5JakartaModule();
+		return hibernate5Moudle;
+	}
 }
