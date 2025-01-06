e1d134c38774c6b6f2a453515ae72d04d447898d
MINU234 <kmw365849@gmail.com>
2025-01-07 02:44:31 +0900

    JPA 스프링부트 실전 2) 페이징과 한계돌파

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -102,4 +102,23 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<Order> findAllWithItem(){ // distinct 중복을 방지하여 sql문 실행해줌
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d"+
+                        " join fetch  o.orderItems oi"+
+                        " join fetch oi.item i",Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset,int limit){ // distinct 중복을 방지하여 sql문 실행해줌
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m"+
+                                " join fetch o.delivery d",Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
